7ff40fba9564212bf298671363880c71448db3c5
Varalakshmi N <475929@cognizant.com>
2019-08-24 01:54:40 -0700

    Added the requied reference

--- ProjectManager.LoadTest/ProjectManagerLoadTest.cs
+++ ProjectManager.LoadTest/ProjectManagerLoadTest.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectManager.Business;
+using ProjectManager.Business.DTO;
+using ProjectManager.Data;
+using AutoMapper;
+using NBench;
 
 namespace ProjectManager.LoadTest
 {
-    public class Class1
+    public class ProjectManagerLoadTest
     {
     }
 }
